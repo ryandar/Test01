f9ad1e5ea084286c8a4f75e409f1d0386b198213
Darren <darren_ryan@hotmail.com>
2015-11-07 16:44:06 +0000

    1644

--- main.c
+++ main.c
@@ -1,16 +1,54 @@
 #include "stdio.h"
 
+char* gNames[100];
+
+void EnterNames();
+void DisplayNames();
+
 void main(void)
 {
-	char c;
 	printf("Starting\n\n");
 
-	printf("Enter character:");
-	c = getch();
-
-	printf("\n");
-	printf("Entered %c\n", c);
+	EnterNames();
+	DisplayNames();
 
 	printf("\nComplete");
 	getch();
+}
+
+void EnterNames()
+{
+	int i = 0;
+
+	while (i < 2)
+	{
+		char input[50] = { 0 };
+		int size = 0;
+		char *name = NULL;
+
+		printf("Enter name: ");
+		scanf("%s", input);
+
+		size = strlen(input) * sizeof(char);
+		name = malloc(size);
+		strcpy(name, input);
+		gNames[i] = name;
+
+		i++;
+	}
+}
+
+void DisplayNames()
+{
+	int i = 0;
+
+	printf("\n");
+	printf("\t Entered \n");
+	printf("\t ---------- \n");
+
+	while (i < 10)
+	{
+		printf("\t %d %s \n", i, gNames[i]);
+		i++;
+	}
 }
