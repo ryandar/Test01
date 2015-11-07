6f94f0f1854a848211d7c66263626989553ebd44
Darren <darren_ryan@hotmail.com>
2015-11-07 17:48:04 +0000

    1644

--- main.c
+++ main.c
@@ -1,54 +1,109 @@
 #include "stdio.h"
+#include "stdlib.h"
 
-char* gNames[100];
+typedef enum { false, true } bool;
 
-void EnterNames();
-void DisplayNames();
+char* gInput[100];
+char* gCache[10];
+int gInputIndex = 0;
+
+bool EnterName();
+void Display();
+void ClearScreen();
 
 void main(void)
 {
-	printf("Starting\n\n");
+	Display();
 
-	EnterNames();
-	DisplayNames();
-
-	printf("\nComplete");
-	getch();
-}
-
-void EnterNames()
-{
-	int i = 0;
-
-	while (i < 2)
+	while (EnterName())
 	{
-		char input[50] = { 0 };
-		int size = 0;
-		char *name = NULL;
-
-		printf("Enter name: ");
-		scanf("%s", input);
-
-		size = strlen(input) * sizeof(char);
-		name = malloc(size);
-		strcpy(name, input);
-		gNames[i] = name;
-
-		i++;
+		ClearScreen();
+		Display();
 	}
 }
 
-void DisplayNames()
+bool EnterName()
+{
+	char input[50] = { 0 };
+	int size = 0;
+	char *name = NULL;
+
+	printf("\n");
+	printf("Enter name or X to exit: ");
+	scanf("%s", input);
+
+	if (!strcmp("X",input))
+	{
+		return false;
+	}
+
+	size = strlen(input) * sizeof(char);
+	name = malloc(size);
+	strcpy(name, input);
+	gInput[gInputIndex] = name;
+
+	gInputIndex++;
+
+	return true;
+}
+
+void Display()
 {
 	int i = 0;
 
 	printf("\n");
-	printf("\t Entered \n");
-	printf("\t ---------- \n");
+	printf("\t");
+	printf("\t");
+	printf("Input");
+	printf("\t");
+	printf("\t");
+	printf("Cache");
+	printf("\n");
+	printf("\t");
+	printf("\t");
+	printf("-----");
+	printf("\t");
+	printf("\t");
+	printf("-----");
+	printf("\n");
 
-	while (i < 10)
+	while (i < 15)
 	{
-		printf("\t %d %s \n", i, gNames[i]);
+		printf("\t");
+		printf("%d", i);
+		printf("\t");
+
+		if (gInput[i] == NULL)
+		{
+			printf("-");
+		}
+		else
+		{
+			printf("%s", gInput[i]);
+		}
+
+		printf("\t");
+		printf("\t");
+
+		if (i < 10)
+		{
+			if (gCache[i] == NULL)
+			{
+				printf("-");
+			}
+			else
+			{
+				printf("%s", gCache[i]);
+			}
+		}
+
+		printf("\n");
 		i++;
 	}
+}
+
+
+void ClearScreen()
+{
+	system("cls");
 }
